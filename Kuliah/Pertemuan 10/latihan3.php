e82965c1310f79089cca12dbc38e410f036cd76d
FahriArli11 <FahriArli11@github.com>
2020-04-23 10:03:16 +0700

    telah menambahkan img

--- Kuliah/Pertemuan 10/latihan3.php
+++ Kuliah/Pertemuan 10/latihan3.php
@@ -14,7 +14,7 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
 <body>
   <h3>Daftar Mahasiswa</h3>
 
-  <a href="">Tambah Data Mahasiswa</a>
+  <a href="tambah.php">Tambah Data Mahasiswa</a>
   <br><br>
 
   <table border="1" cellpadding="10" cellspacing="0">
